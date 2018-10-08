5a090d3b2d3e35ae9c22ecbd1d255c72a06a497f
Putt Sakdhnagool <15830559+puttsk@users.noreply.github.com>
2018-10-08 14:36:08 +0700

    Update config

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'NSTDA-SCDocumentationdoc'
+htmlhelp_basename = 'ThaiSCDocumentationdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -136,7 +136,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'NSTDA-SCDocumentation.tex', 'NSTDA-SC Documentation Documentation',
+    (master_doc, 'ThaiSCDocumentation.tex', 'ThaiSC Documentation',
      'Putt Sakdhnagool', 'manual'),
 ]
 
@@ -146,7 +146,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'nstda-scdocumentation', 'NSTDA-SC Documentation Documentation',
+    (master_doc, 'thaisc-documentation', 'ThaiSC Documentation',
      [author], 1)
 ]
 
@@ -157,8 +157,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'NSTDA-SCDocumentation', 'NSTDA-SC Documentation Documentation',
-     author, 'NSTDA-SCDocumentation', 'One line description of project.',
+    (master_doc, 'ThaiSCDocumentation', 'ThaiSC Documentation',
+     author, 'ThaiSCDocumentation', 'One line description of project.',
      'Miscellaneous'),
 ]
 
